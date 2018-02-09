1a279b126088a151243eeb3a075355cb259005a4
Marc Baldwin <marc.baldwin88@gmail.com>
2018-02-09 09:39:27 +0000

    Initial Commit

---
+++ UIKitExt/UIKitExt.h
@@ -0,0 +1,19 @@
+//
+//  UIKitExt.h
+//  UIKitExt
+//
+//  Created by Marc Baldwin on 09/02/2018.
+//  Copyright © 2018 Marc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitExt.
+FOUNDATION_EXPORT double UIKitExtVersionNumber;
+
+//! Project version string for UIKitExt.
+FOUNDATION_EXPORT const unsigned char UIKitExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitExt/PublicHeader.h>
+
+
