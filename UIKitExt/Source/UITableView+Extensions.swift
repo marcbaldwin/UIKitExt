0f6f984e02ad074526e6172d9311bdaa33d2a317
Marc Baldwin <marc.baldwin88@gmail.com>
2018-08-07 15:38:15 +0100

    Add convenience functions to register and dequeue cells

--- UIKitExt/Source/UITableView+Extensions.swift
+++ UIKitExt/Source/UITableView+Extensions.swift
@@ -11,4 +11,28 @@ public extension UITableView {
     public func scrollToTop(animated: Bool = false) {
         scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: animated)
     }
+
+    public func registerClassesForCellReuse(_ cellClasses: UITableViewCell.Type...) {
+        cellClasses.forEach { registerClassForCellReuse($0) }
+    }
+
+    public func registerClassesForCellReuse(_ cellClasses: [UITableViewCell.Type]) {
+        cellClasses.forEach { registerClassForCellReuse($0) }
+    }
+
+    public func registerClassForCellReuse(_ cellClass: UITableViewCell.Type) {
+        register(cellClass, forCellReuseIdentifier: "\(cellClass)")
+    }
+
+    public func dequeueReusableCell<T>(class cellClass: T.Type) -> T {
+        return dequeueReusableCell(withIdentifier: "\(cellClass)")! as! T
+    }
+
+    public func registerClassForHeaderFooterViewReuse(_ aClass: UIView.Type) {
+        register(aClass, forHeaderFooterViewReuseIdentifier: "\(aClass)")
+    }
+
+    public func dequeueReusableHeaderFooterView(class aClass: UIView.Type) -> UIView? {
+        return dequeueReusableHeaderFooterView(withIdentifier: "\(aClass)")
+    }
 }
